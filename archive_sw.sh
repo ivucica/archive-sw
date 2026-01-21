d918152ea8d44e136e0771de54ed4d0da1cf5d5e
Ivan Vučica <ivan@vucica.net>
2026-01-21 18:38:05 +0000

    archive_sw.sh: Suggested commands for using gh-org-repos have been updated to use different envvar (not USER), to have JOBS, and to have different joblogs.

--- archive_sw.sh
+++ archive_sw.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Example:
-# ~/zzz-gh-org-repos.sh $USER | REPO_UPDATE=1 parallel --joblog /tmp/job_progress.log --resume --resume-failed -P3 --bar -I{} ~/zzz-archive_sw.sh https://github.com/$USER/{}
+# GHORG=something
+# JOBS=3
+# ~/zzz-gh-org-repos.sh $GHORG | REPO_UPDATE=1 parallel --joblog /tmp/$GHORG_job_progress.log --resume --resume-failed -P${JOBS:-3} --bar -I{} ~/zzz-archive_sw.sh https://github.com/$GHORG/{}
 # or
-# curl https://api.github.com/users/$USER/repos?per_page=100 | jq -r '.[] | select(.disabled != true) | .name'   | REPO_UPDATE=1 parallel --joblog /tmp/$USER_job_progress.log --resume --resume-failed -P1 --bar -I{} ~/zzz-archive_sw.sh https://github.com/$USER/{}
+# curl https://api.github.com/users/$GHORG/repos?per_page=100 | jq -r '.[] | select(.disabled != true) | .name'   | REPO_UPDATE=1 parallel --joblog /tmp/$GHORG_job_progress.log --resume --resume-failed -P${JOBS:-3} --bar -I{} ~/zzz-archive_sw.sh https://github.com/$GHORG/{}
 
 set -e
 POOL="${POOL:-"$(ls -1 / | grep pool | head -n1)"}"
